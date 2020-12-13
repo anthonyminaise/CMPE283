f4c1e0af25130f54c36e973ce32d821d395a6671
Anthony Minaise <anthonyminaise97@gmail.com>
2020-12-12 21:22:29 -0800

    Update vmx.c
    forgot semi colons

--- Assignment 2/vmx.c
+++ Assignment 2/vmx.c
@@ -5939,9 +5939,9 @@ static int vmx_handle_exit(struct kvm_vcpu *vcpu, fastpath_t exit_fastpath)
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
 	u32 exit_reason = vmx->exit_reason;
 	u32 vectoring_info = vmx->idt_vectoring_info;
-	uint64_t start_time = rdtsc()
+	uint64_t start_time = rdtsc();
 
-	arch_atomic64_inc(&exit_counter)
+	arch_atomic64_inc(&exit_counter);
 
 	/*
 	 * Flush logged GPAs PML buffer, this will make dirty_bitmap more
